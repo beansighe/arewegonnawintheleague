efb1a65b01e7160d77c2ec9806528884b65eebf8
Tierney McBride <tierney6@pdx.edu>
2025-03-18 19:46:39 -0700

    data reading functional

--- gonnawintheleague/src/lib.rs
+++ gonnawintheleague/src/lib.rs
@@ -3,48 +3,44 @@
 //use chrono::{Datelike, NaiveDate, Timelike, Utc};
 use rand::distr::weighted::WeightedIndex;
 use rand::prelude::*;
-//use reqwest::blocking;
-//use scraper::{Html, Selector};
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
 use std::collections::HashMap;
-use std::fs;
-use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::env::current_dir;
+use relative_path::RelativePath;
 
-
-//source for distribution calcuation:
+//source for weights calcuation for use as distribution model in Monte Carlo simulation:
 //    https://fivethirtyeight.com/features/in-126-years-english-football-has-seen-13475-nil-nil-draws/
 
 const NUM_POSSIBLE_GOALS: [i32; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
 const HOME_WEIGHTS: [f32; 8] = [18.8, 30.3, 24.8, 14.3, 7.0, 3.1, 1.2, 0.5];
 const AWAY_WEIGHTS: [f32; 8] = [33.8, 36.2, 19.3, 7.4, 2.3, 0.7, 0.2, 0.1];
-//const API_URL: &str = "https://v3.football.api-sports.io/standings";
-//const FIXTURES_URL: &str = "https://www.theguardian.com/football/premierleague/fixtures";
-//const FIXTURES_URL: &str =
-    //"https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures";
+const FIXTURES_PATH: &str = "/files/fixtures_list.json";
+const STANDINGS_PATH: &str = "/files/standings.json";
 
 // Structures for managing data within simulations
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // struct to store individual team data
 // held within the league table structure
-#[derive(Debug, Default, Clone, PartialEq)]
+#[derive(Debug, Default, Clone, PartialEq, Deserialize, Serialize)]
 pub struct Team {
     name: String,
     pts: u32,
     goal_diff: i32,
-    matches_played: u32,
 }
 
 impl Team {
-    pub fn new(name: String, pts: u32, goal_diff: i32, matches_played: u32) -> Self {
+    pub fn new(name: String, pts: u32, goal_diff: i32) -> Self {
         Self {
             name,
             pts,
             goal_diff,
-            matches_played,
         }
     }
 
     pub fn update(&mut self, match_goal_diff: i32) {
-        self.matches_played += 1;
         self.goal_diff += match_goal_diff;
         if match_goal_diff == 0 {
             self.pts += 1;
@@ -85,12 +81,8 @@ impl LeagueTable {
         Self::default()
     }
 
-    //    pub fn from() -> Self {
-
-    //   }
-
     pub fn print_table(&self) {
-        println!("Rank\tTeam");
+        println!("Rank\tTeam\t\t\tPoints\t GD");
         let mut i = 1;
         let mut print_vector: Vec<&Team> = self.0.values().collect();
         print_vector.sort_by(|x, y| {
@@ -99,25 +91,19 @@ impl LeagueTable {
                 .then_with(|| y.goal_diff.cmp(&x.goal_diff))
         });
         for team in print_vector {
-            print!("{}\t{}\n", i, team.name);
+            println!("{}\t{:<10}\t\t{:>5}\t{:>3}", i, team.name, team.pts, team.goal_diff);
             i += 1;
         }
     }
 
-    pub fn add_team(
-        &mut self,
-        name: String,
-        pts: u32,
-        goals_for: i32,
-        goals_against: i32,
-        matches_played: u32,
-    ) {
-        self.0.entry(name.clone()).insert_entry(Team::new(
-            name.clone(),
-            pts,
-            goals_for - goals_against,
-            matches_played,
-        ));
+    pub fn add_team(&mut self, name: String, pts: u32, goals_diff: i32) {
+        self.0
+            .entry(name.clone())
+            .insert_entry(Team::new(name.clone(), pts, goals_diff));
+    }
+
+    pub fn add_team_struct(&mut self, name: String, team: Team) {
+        self.0.entry(name.clone()).insert_entry(team);
     }
 
     pub fn update(&mut self, latest_match: &Match) {
@@ -159,7 +145,7 @@ impl LeagueTable {
 pub fn run_simulation(
     target_team: &String,
     current_table: &mut LeagueTable,
-    match_list: &mut [Match],
+    match_list: &mut Vec<Match>,
 ) -> i32 {
     let mut simulated_table = current_table.clone();
     let home_dist = WeightedIndex::new(&HOME_WEIGHTS).unwrap();
@@ -176,94 +162,43 @@ pub fn run_simulation(
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// API interfacing
-
-/*pub fn get_standings(client: &mut Client) {
-    let get_key= env::var("API_SPORTS_KEY");
-    if get_key.is_ok() {
-        let key = get_key.unwrap();
-        let resp = client.get(API_URL).header("x-rapidapi-key", key).query(&[("league", "39"), ("season", "2024"),]).send();
-        if resp.is_ok() {
-            let results = resp.unwrap();
-            print!("results: {:?}", results);
-        }
-    }
-}
-    */
-
-// web scraping
-/*
-
-pub fn scraper_test() {
-    let response = reqwest::blocking::get(FIXTURES_URL);
-    let html_content = response.unwrap().text().unwrap();
-    let parsed = Html::parse_document(&html_content);
-    let now = Utc::now().date_naive();
-    let date_selector = Selector::parse(r#"td[data-stat="date"]"#).unwrap();
-    for element in parsed.select(&date_selector).by_ref() {
-        let date_result =
-            NaiveDate::parse_from_str(&element.text().collect::<String>(), "%Y-%m-%d");
-        match date_result {
-            Ok(date) => {
-                if date > now {
-                    println!("date: {:?}", date);
-                }
-            }
-            Err(_e) => (),
-        }
-    }
-}
-
-pub fn scrape_fixtures(match_list: &mut Vec<Match>) {
-    let now = Utc::now().date_naive();
-    let response = reqwest::blocking::get(FIXTURES_URL);
-    let html_content = response.unwrap().text().unwrap();
-    let parsed = Html::parse_document(&html_content);
-    let selector = Selector::parse("tr").unwrap();
-    let date_selector = Selector::parse(r#"td[data-stat="date"]"#).unwrap();
-    let home_team_selector = Selector::parse(r#"td[data-stat="home_team"]"#).unwrap();
-    let away_team_selector = Selector::parse(r#"td[data-stat="away_team"]"#).unwrap();
-    let mut skip = false;
-    for element in parsed.select(&selector) {
-        if skip == false {
-            for sub_element in element.select(&date_selector).by_ref() {
-                let date_result =
-                    NaiveDate::parse_from_str(&sub_element.text().collect::<String>(), "%Y-%m-%d");
-                match date_result {
-                    Ok(date) => {
-                        if date > now {
-                            //println!("element: {:?}", element);
-                            //println!("sub_element: {:?}", sub_element);
-                            skip = true;
-                            break;
-                        }
-                    }
-                    Err(_e) => (),
-                }
-            }
-        }
-        for sub_element in element.select(&home_team_selector).by_ref() {
-            //check if it exists
-            println!("home team: {:?}", sub_element.text().collect::<String>());
-        }
-    }
-}
-    */
-
-
 // read in data from files
 
-pub fn read_fixtures() -> Result<(), Box<dyn Error>> {
-    let fixtures = fs::read_to_string("files/fixtures_list.json")?;
-    println!("{fixtures:?}");
-    Ok(())
+pub fn read_fixtures(fixture_list: &mut Vec<Match>) {
+    let root_dir = current_dir().expect("should only be run in valid directory with appropriate permissions");
+    let fixtures_relative= RelativePath::new(FIXTURES_PATH);
+    let fixtures_full_path = fixtures_relative.to_path(&root_dir);
+    println!("fixtures path: {fixtures_full_path:?}");
+    let file = File::open(fixtures_full_path).expect("file should open if path constant valid");
+    let reader = BufReader::new(file);
+    let fixtures: Result<Value> = serde_json::from_reader(reader);
+    match fixtures {
+        Ok(list) => {
+            for i in 0..379 {
+                let catch = list.get(i);
+                match catch {
+                    None => break,
+                    Some(entry) => {
+                        fixture_list.push(Match::from(entry["home"].as_str().unwrap(), entry["away"].as_str().unwrap()));
+                    }
+                }
+            }
+        },
+        Err(error) => println!("error reading file: {error:?}"),
+    }
 }
 
-
-pub fn read_standings() -> Result<(), Box<dyn Error>> {
-    let standings = fs::read_to_string("files/standings.json")?;
-    println!("{standings:?}");
-    Ok(())
+pub fn read_standings(current_table: &mut LeagueTable) {
+    let root_dir = current_dir().expect("should only be run in valid directory with appropriate permissions");
+    let standings_relative= RelativePath::new(STANDINGS_PATH);
+    let standings_full_path = standings_relative.to_path(&root_dir);
+    println!("standings full path: {standings_full_path:?}");
+    let file = File::open(standings_full_path).expect("file should open if path constant valid");
+    let reader = BufReader::new(file);
+    let standings_data: [Team; 20] = serde_json::from_reader(reader).expect("data should be correctly formatted");
+    for team in standings_data {
+        current_table.add_team_struct(team.name.to_string(), team.clone());
+    }
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -275,7 +210,7 @@ mod tests {
     #[test]
     fn add_one_team() {
         let mut league_table = LeagueTable::new();
-        league_table.add_team("Liverpool".to_string(), 67, 66, 26, 28);
+        league_table.add_team("Liverpool".to_string(), 67, 40);
         assert_ne!(league_table.0.get("Liverpool"), None);
         assert_eq!("Liverpool", league_table.0.get("Liverpool").unwrap().name);
     }
@@ -283,16 +218,16 @@ mod tests {
     #[test]
     fn print_league_table() {
         let mut league_table = LeagueTable::new();
-        league_table.add_team("Liverpool".to_string(), 67, 66, 26, 28);
-        league_table.add_team("Arsenal".to_string(), 27, 51, 23, 27);
+        league_table.add_team("Liverpool".to_string(), 67, 40);
+        league_table.add_team("Arsenal".to_string(), 27, 28);
         league_table.print_table();
     }
 
     #[test]
     fn print_reranked_league_table() {
         let mut league_table = LeagueTable::new();
-        league_table.add_team("Liverpool".to_string(), 67, 66, 26, 28);
-        league_table.add_team("Arsenal".to_string(), 27, 51, 23, 27);
+        league_table.add_team("Liverpool".to_string(), 67, 40);
+        league_table.add_team("Arsenal".to_string(), 27, 28);
         league_table.print_table();
 
         league_table
@@ -305,11 +240,10 @@ mod tests {
     #[test]
     fn manually_update_team_data() {
         let mut league_table = LeagueTable::new();
-        league_table.add_team("Liverpool".to_string(), 67, 66, 26, 28);
+        league_table.add_team("Liverpool".to_string(), 67, 40);
         assert_ne!(league_table.0.get("Liverpool"), None);
         assert_eq!(67, league_table.0.get("Liverpool").unwrap().pts);
         assert_eq!(40, league_table.0.get("Liverpool").unwrap().goal_diff);
-        assert_eq!(28, league_table.0.get("Liverpool").unwrap().matches_played);
     }
 
     #[test]
@@ -321,17 +255,15 @@ mod tests {
             away_goals: 0,
         };
         let mut league_table = LeagueTable::new();
-        league_table.add_team("Liverpool".to_string(), 67, 66, 26, 28);
-        league_table.add_team("Arsenal".to_string(), 27, 51, 23, 27);
+        league_table.add_team("Liverpool".to_string(), 67, 40);
+        league_table.add_team("Arsenal".to_string(), 27, 26);
         league_table.update(&new_match);
 
         assert_eq!(70, league_table.0.get("Liverpool").unwrap().pts);
         assert_eq!(42, league_table.0.get("Liverpool").unwrap().goal_diff);
-        assert_eq!(29, league_table.0.get("Liverpool").unwrap().matches_played);
 
         assert_eq!(27, league_table.0.get("Arsenal").unwrap().pts);
         assert_eq!(26, league_table.0.get("Arsenal").unwrap().goal_diff);
-        assert_eq!(28, league_table.0.get("Arsenal").unwrap().matches_played);
 
         let second_match = Match {
             home: "Liverpool".to_string(),
@@ -343,18 +275,16 @@ mod tests {
 
         assert_eq!(71, league_table.0.get("Liverpool").unwrap().pts);
         assert_eq!(42, league_table.0.get("Liverpool").unwrap().goal_diff);
-        assert_eq!(30, league_table.0.get("Liverpool").unwrap().matches_played);
 
         assert_eq!(28, league_table.0.get("Arsenal").unwrap().pts);
         assert_eq!(26, league_table.0.get("Arsenal").unwrap().goal_diff);
-        assert_eq!(29, league_table.0.get("Arsenal").unwrap().matches_played);
     }
 
     #[test]
     fn get_final_ranking() {
         let mut league_table = LeagueTable::new();
-        league_table.add_team("Liverpool".to_string(), 67, 66, 26, 28);
-        league_table.add_team("Arsenal".to_string(), 54, 51, 23, 27);
+        league_table.add_team("Liverpool".to_string(), 67, 40);
+        league_table.add_team("Arsenal".to_string(), 54, 28);
 
         let liverpool_rank = league_table.find_final_rank("Liverpool");
         let arsenal_rank = league_table.find_final_rank("Arsenal");
@@ -366,12 +296,12 @@ mod tests {
     #[test]
     fn small_simulation() {
         let mut league_table = LeagueTable::new();
-        league_table.add_team("Liverpool".to_string(), 67, 66, 26, 28);
-        league_table.add_team("Arsenal".to_string(), 54, 51, 23, 27);
-        league_table.add_team("Nottingham Forest".to_string(), 48, 44, 26, 27);
-        league_table.add_team("Manchester City".to_string(), 47, 53, 37, 27);
+        league_table.add_team("Liverpool".to_string(), 67, 40);
+        league_table.add_team("Arsenal".to_string(), 54, 28);
+        league_table.add_team("Nottingham Forest".to_string(), 48, 18);
+        league_table.add_team("Manchester City".to_string(), 47, 16);
 
-        let mut matches = [
+        let mut matches = vec![
             Match::from("Liverpool", "Arsenal"),
             Match::from("Liverpool", "Nottingham Forest"),
             Match::from("Liverpool", "Manchester City"),
@@ -397,29 +327,18 @@ mod tests {
         println!("{} {}%", target, count / 50.0 * 100.0);
     }
 
-    /*     #[test]
-    fn api_test() {
-        let mut client = Client::new();
-        get_standings(&mut client);
-    }
-    */
-   /* #[test]
-    fn scraping_test() {
-        let mut fixtures = Vec::<Match>::new();
-        //scrape_fixtures(&mut fixtures);
-        //scraper_test();
-    }
-    */
 
     #[test]
     fn read_in_table() {
-        read_standings();
+        let mut new_league_table = LeagueTable::new();
+        read_standings(&mut new_league_table);
+        new_league_table.print_table();
     }
 
     #[test]
     fn read_in_fixture_list() {
-        read_fixtures();
+        let mut fixtures_list = Vec::<Match>::new();
+        read_fixtures(&mut fixtures_list);
+        println!("Fixtures\n{fixtures_list:?}");
     }
-
-
 }
